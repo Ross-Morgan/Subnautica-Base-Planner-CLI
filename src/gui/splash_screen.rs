5a323e5647f591ae7600a7b8cb149371d9d42652
Ross-Morgan <morgan.ross.512@gmail.com>
2025-01-08 22:54:00 +0000

    move image loading to after splash screen creation

--- src/gui/splash_screen.rs
+++ src/gui/splash_screen.rs
@@ -1,9 +1,6 @@
 use std::{collections::HashMap, thread::JoinHandle};
 
-use eframe::{
-    egui::{self, load::SizedTexture, ColorImage, Image, TextureHandle},
-    CreationContext,
-};
+use eframe::egui::{self, load::SizedTexture, ColorImage, Image, TextureHandle};
 use strum::IntoEnumIterator;
 
 use crate::subnautica::biomes::Biome;
@@ -12,15 +9,42 @@ const TO_COMPLETE: u8 = 22;
 
 pub struct SplashScreen<'a> {
     completed: u8,
-    splash_image: (TextureHandle, Image<'static>),
+    splash_biome: Biome,
+    splash_image: Option<(TextureHandle, Image<'static>)>,
     handles: HashMap<Biome, Option<JoinHandle<ColorImage>>>,
     images: &'a mut HashMap<Biome, ColorImage>,
 }
 
 impl<'a> eframe::App for SplashScreen<'a> {
     fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
+        self.splash_image.get_or_insert_with(|| {
+            let rgb = image::open(self.splash_biome.associated_path())
+                .expect("Couldn't load image (App:run)")
+                .to_rgb8();
+
+            let color_image = ColorImage::from_rgb([1920, 1080], &rgb);
+            let handle = ctx.load_texture(self.splash_biome.to_string(), color_image, egui::TextureOptions::LINEAR);
+            let texture = SizedTexture::new(handle.id(), egui::vec2(1920.0, 1080.0));
+            let image = Image::from_texture(texture);
+            let splash_image = (handle, image);
+
+            for biome in Biome::iter() {
+                let handle = std::thread::spawn(move || {
+                    let rgb = image::open(biome.associated_path())
+                        .expect("Couldn't load image (App:run)")
+                        .to_rgb8();
+                    let color_image = ColorImage::from_rgb([1920, 1080], &rgb);
+                    color_image
+                });
+
+                self.handles.insert(biome, Some(handle));
+            }
+
+            splash_image
+        });
+
         egui::CentralPanel::default().show(ctx, |ui| {
-            self.splash_image.1.paint_at(ui, ui.ctx().screen_rect());
+            self.splash_image.as_ref().unwrap().1.paint_at(ui, ui.ctx().screen_rect());
             self.completed = self.handles.iter().fold(0, |acc, (_, b)| {
                 acc + b.as_ref().map(|j| j.is_finished() as u8).unwrap_or(0)
             });
@@ -44,44 +68,21 @@ impl<'a> eframe::App for SplashScreen<'a> {
                 .insert(biome, handle.take().unwrap().join().expect("Thread failed"));
         }
     }
+
 }
 
 impl<'a> SplashScreen<'a> {
     pub fn new(
-        biome: Biome,
-        cc: &CreationContext,
+        splash_biome: Biome,
         images: &'a mut HashMap<Biome, ColorImage>,
     ) -> Self {
-        let rgb = image::open(biome.associated_path())
-            .expect("Couldn't load image (App:run)")
-            .to_rgb8();
-        let color_image = ColorImage::from_rgb([1920, 1080], &rgb);
-        let handle =
-            cc.egui_ctx
-                .load_texture(biome.to_string(), color_image, egui::TextureOptions::LINEAR);
-        let texture = SizedTexture::new(handle.id(), egui::vec2(1920.0, 1080.0));
-        let image = Image::from_texture(texture);
-        let splash_image = (handle, image);
 
-        let mut app = Self {
+        Self {
             completed: 0,
-            splash_image,
+            splash_image: None,
+            splash_biome,
             handles: HashMap::new(),
             images,
-        };
-
-        for biome in Biome::iter() {
-            let handle = std::thread::spawn(move || {
-                let rgb = image::open(biome.associated_path())
-                    .expect("Couldn't load image (App:run)")
-                    .to_rgb8();
-                let color_image = ColorImage::from_rgb([1920, 1080], &rgb);
-                color_image
-            });
-
-            app.handles.insert(biome, Some(handle));
         }
-
-        app
     }
 }
