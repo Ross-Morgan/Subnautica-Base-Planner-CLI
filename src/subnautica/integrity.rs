e2c64243c4768eb215f4ae647ade4132129b74cd
Ross-Morgan <morgan.ross.512@gmail.com>
2025-01-08 23:26:01 +0000

    merge redundant match branches

--- src/subnautica/integrity.rs
+++ src/subnautica/integrity.rs
@@ -10,24 +10,19 @@ impl Integrity for Item {
         match *self {
             Item::Bulkhead => 3.0,
             Item::Foundation => 2.0,
-            Item::GlassCompartment => -2.0,
-            Item::TitaniumCompartment => -1.0,
-            Item::Hatch => -1.0,
+            Item::TitaniumCompartment | Item::Hatch | Item::Window | Item::ScannerRoom => -1.0,
             Item::Moonpool => -5.0,
             Item::Observatory => -3.0,
             Item::Reinforcement => 7.0,
-            Item::ScannerRoom => -1.0,
             Item::VerticalConnector => -0.5,
-            Item::WaterFiltrationMachine => -2.0,
-            Item::Window => -1.0,
             Item::MultipurposeRoom => -1.25,
-            Item::MultipurposeRoomGlassRoof => -2.0,
-            Item::LargeRoom => -4.0,
-            Item::LargeRoomGlassRoof => -4.0,
+            Item::GlassCompartment | Item::WaterFiltrationMachine | Item::MultipurposeRoomGlassRoof => -2.0,
+            Item::LargeRoom | Item::LargeRoomGlassRoof => -4.0,
         }
     }
 }
 
+#[allow(clippy::cast_precision_loss, clippy::cast_sign_loss)]
 impl Integrity for Base {
     fn integrity(&self) -> f64 {
         let negative_integrity = self
@@ -51,7 +46,7 @@ impl Integrity for Base {
         let multiplier = match self.depth {
             0 => 0.0,
             1..=99 => 1.0,
-            depth => ((depth - 100) / 1000) as f64 + 1.0,
+            depth => f64::from((depth - 100) / 1000) + 1.0,
         };
 
         ((negative_integrity + 10.0) * multiplier) + positive_integrity
